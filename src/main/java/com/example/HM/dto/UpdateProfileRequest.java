94e0d68377d32fe498660152ce735f55dcdd96a2
Thanh2004-coder <thanh200417@gmail.com>
2026-02-02 19:52:25 +0700

    update reser password và forgot password
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/HM/config/EmailConfig.java
    #	src/main/java/com/example/HM/dto/AccountDTO.java
    #	src/main/java/com/example/HM/entity/BaseEntity.java
    #	src/main/java/com/example/HM/repository/AccountRepository.java
    #	src/main/java/com/example/HM/service/AccountService.java
    #	src/main/java/com/example/HM/service/impl/AccountServiceImpl.java
    #	src/main/java/com/example/HM/service/impl/EmailServiceImpl.java
    #	src/main/resources/templates/admin/account-list.html
    #	src/main/resources/templates/fragments/sidebar.html
    #	src/main/resources/templates/index.html
    #	src/main/resources/templates/layout/main_layout.html

---
+++ src/main/java/com/example/HM/dto/UpdateProfileRequest.java
@@ -0,0 +1,11 @@
+package com.example.HM.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateProfileRequest {
+    private String fullName;
+    private String idNumber;
+    private String idType;
+    private String nationality;
+}
